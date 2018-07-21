79d903ee85bac1d71a5eca9cd8400eefb9e5be09
Maurizio <zeta_persei@hotmail.com>
2018-07-21 17:33:06 +0200

    Update checkip.sh

--- checkip.sh
+++ checkip.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
-user="user"
-
-ip_old="`tail /home/$user/.infoip`"
+user="user" ip_old="`tail /home/$user/.infoip`" 
 ip_now="`wget -qO- http://checkip.dyndns.org/ | sed "s/.* \([[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\).*/\1/g"`"
 echo $ip_now > /home/$user/.infoip
-if [ "$ip_old" != "$ip_now" ]; then
-titoloip="Public IP chenged!"
+if [ "$ip_old" != "$ip_now" ];
+then
+titoloip="Public IP changed!"
 ipresult="The new IP is $ip_now"
-echo $ipresult | mail -s "IP Public" user@dominie.xx 
+echo $titoloip $ipresult | mail -s "IP Public" <user@domain>
 fi
-
 exit
+
